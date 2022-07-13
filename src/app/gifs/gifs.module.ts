e77b829ec66ae1702054d3ae61e35cffc6d7bd91
Juan Diego Espinosa Santos <juandiegoespinosasantos@gmail.com>
2022-07-12 22:44:36 -0500

    ViewChild

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+
 import { PageComponent } from "./page/page.component";
 import { SearchComponent } from "./search/search.component";
-import { ResultsComponent } from './results/results.component';
+import { ResultsComponent } from "./results/results.component";
 
 @NgModule({
     declarations: [
--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
 @Component({
     selector: "app-search",
@@ -8,9 +8,20 @@ export class SearchComponent implements OnInit {
 
     public label: string = "Buscar";
     public placeholder: string = "Buscar GIFs...";
-    
+
+    @ViewChild("txtSearch")
+    public txtSearch!: ElementRef<HTMLInputElement>;
+
     constructor() { }
 
     ngOnInit(): void {
+    }
+
+    public search(): void {
+        const value = this.txtSearch.nativeElement.value;
+
+        console.log(value);
+
+        this.txtSearch.nativeElement.value = "";
     }
 }
