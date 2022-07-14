6e8236ec4dffdd53a89e55e7e03227f70a1d6028
Juan Diego Espinosa Santos <juandiegoespinosasantos@gmail.com>
2022-07-13 21:09:24 -0500

    Loading animations

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 import { Gif, GiphyResponse } from "../interfaces/gifs.interface";
 
 const HISTORY_LIMIT: number = 10;
-const GIPHY_API_BASE_URL: string = "http://api.giphy.com/v1/gifs/search";
+const GIPHY_API_BASE_URL: string = "http://api.giphy.com/v1/gifs";
 const GIPHY_API_KEY: string = "WO4lPnBlJqabTGbxmfQLVf3IBvHZ34qz";
 const LOCAL_STORAGE_KEY_SEARCH_PARAM = "searchParam";
 const LOCAL_STORAGE_KEY_HISTORY = "history";
@@ -34,7 +34,7 @@ export class GifsService {
         return [...this._results];
     }
 
-    public searchGifs(searchParam: string = ""): void {
+    public searchGifs(searchParam: string = ""): any {
         searchParam = searchParam.trim().toUpperCase();
 
         if (searchParam.length == 0) return;
@@ -49,18 +49,7 @@ export class GifsService {
         this.saveData(LOCAL_STORAGE_KEY_SEARCH_PARAM, this._searchParam);
         this.saveData(LOCAL_STORAGE_KEY_HISTORY, this._history);
 
-        this.httpClient
-            .get<GiphyResponse>(GIPHY_API_BASE_URL, {
-                params: new HttpParams()
-                    .set("api_key", GIPHY_API_KEY)
-                    .set("q", searchParam)
-                    .set("limit", HISTORY_LIMIT)
-            }).subscribe((resp: GiphyResponse) => {
-                console.log(resp);
-
-                this._results = resp.data;
-                this.saveData(LOCAL_STORAGE_KEY_RESULTS, this._results);
-            });
+        this.getGifs(searchParam);
     }
 
     private loadData(): void {
@@ -73,5 +62,25 @@ export class GifsService {
 
     private saveData(key: string, value: any): void {
         localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    private getGifs(searchParam: string): any {
+        const url = GIPHY_API_BASE_URL + "/search";
+        const params: HttpParams = new HttpParams()
+            .set("api_key", GIPHY_API_KEY)
+            .set("q", searchParam)
+            .set("limit", HISTORY_LIMIT);
+
+        this.httpClient
+            .get<GiphyResponse>(url, {
+                params: params
+            }).subscribe((resp: GiphyResponse) => {
+                console.log(resp);
+
+                this._results = resp.data;
+                this.saveData(LOCAL_STORAGE_KEY_RESULTS, this._results);
+
+                return resp;
+            });
     }
 }
