121b9498dd90619a12e511be79b1856b33844be6
Juan Diego Espinosa Santos <juandiegoespinosasantos@gmail.com>
2022-07-13 19:37:02 -0500

    GIPHY API request

--- src/app/gifs/results/results.component.ts
+++ src/app/gifs/results/results.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { GifsService } from "../services/gifs.service";
 
 @Component({
     selector: "app-results",
@@ -6,11 +7,12 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ResultsComponent implements OnInit {
 
-    public lipsum: string = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex animi eveniet, sint
-     ea soluta sed eum suscipit repellat minus nobis ad iste vero libero vitae officiis, corrupti aliquam consequatur!`;
-
-    constructor() { }
+    constructor(private gifsService: GifsService) { }
 
     ngOnInit(): void {
+    }
+
+    public get results(): any[] {
+        return this.gifsService.results;
     }
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,6 +1,9 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 const historyLimit: number = 10;
+const giphyAPIBaseUrl: string = "http://api.giphy.com/v1/gifs/search";
+const giphyAPIKey: string = "WO4lPnBlJqabTGbxmfQLVf3IBvHZ34qz";
 
 @Injectable({
     providedIn: "root"
@@ -8,22 +11,42 @@ const historyLimit: number = 10;
 export class GifsService {
 
     private _history: string[] = [];
+    private _results: any[] = [];
+
+    constructor(private httpClient: HttpClient) {
+    }
 
     public get history(): string[] {
         return [...this._history];
     }
 
+    public get results(): any[] {
+        return [...this._results];
+    }
+
     public searchGifs(value: string = ""): void {
-        value = value.trim().toUpperCase(); // Lo paso a mayúsculas y sin espacios
+        value = value.trim().toUpperCase();
 
-        if (value.length == 0) return; // Si está vacío no continúo
+        if (value.length == 0) return;
 
-        if (this._history.includes(value)) return; // Si ya está almacenado no cotinúo
+        if (this._history.includes(value)) return;
 
-        if (this._history.length === historyLimit) this._history.pop(); // Antes de insertar elimino el último item si ya llegué al límite
+        if (this._history.length === historyLimit) this._history.pop();
 
         this._history.unshift(value);
 
         console.log(this._history);
+
+        this.httpClient
+            .get(giphyAPIBaseUrl, {
+                params: new HttpParams()
+                    .set("api_key", giphyAPIKey)
+                    .set("q", value)
+                    .set("limit", historyLimit)
+            }).subscribe((resp: any) => {
+                console.log(resp);
+
+                this._results = resp.data;
+            });
     }
 }
